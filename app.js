881ef5157e5a91957b09c948b7d3ddb81b79a638
Aaron Gwirtz <imac@iMacs-iMac.attlocal.net>
2020-11-30 22:54:20 -0500

    working journal controller back end server

---
+++ app.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+let express = require ('express');
+let app = express();
+let sequelize = require('./db');
+
+let journal = require('./controllers/journalcontroller');
+let user = require('./controllers/usercontroller');
+
+sequelize.sync();
+//sequelize.sync({force: true})
+app.use(require('./middleware/header'));
+
+app.use(express.json());
+
+app.use('/user', user);
+app.use('/journal', journal)
+
+app.listen(3000, function (){
+    console.log('App is listening on port 3000');
+})
